7cc9cea50f4b6f5e31d44dcf1ab4e07629865bfa
Handy-Kharisma <handykharisma75@gmail.com>
2022-03-15 16:13:54 +0700

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
-
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-    console.log('Hello Server...');
-})
+router.use('/product', function(req, res, next){
+    // console.log('request: ', req);
+    // console.log('url: ', req.originalUrl);
+    // console.log('method: ', req.method);
+    res.json({name : "Handy Kharisma", email : "handykharisma75@gmail.com"});
+    next();
+    // res.send("Hello from the root application URL");
+});
+
+router.use('/price', function(req, res, next){
+    res.json({price : "3000000"});
+    next();
+});
+
+router.get('/customers', function(req, res, next){
+    res.json({title : "Customer"});
+    next();
+});
+
+// router.delete('/customers', function(req, res, next){
+//     res.json({title : "Customer"});
+//     next();
+// });
+
+// app.get('/', function(req, res, next){
+//     res.send("Hello from the root application URL");
+// });
+
+app.use('/', router);
 
 app.listen(4000);
+
+// , () => console.log('Application is running')
