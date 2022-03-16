abc6d51323cd3d7f18880244e4d7dc1739e37515
Handy-Kharisma <handykharisma75@gmail.com>
2022-03-16 13:04:50 +0700

    sixth commit

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,12 +1,14 @@
 exports.createProduct = (function(req, res, next){
-    console.log('request: ', req.body);
+    // console.log('request: ', req.body);
+    const name = req.body.name;
+    const price = req.body.price;
     res.json(
         {
             message: 'Create Product Success !!!',
             data: {
                 id: 1,
-                name: 'Sari Gandum',
-                price: 8000
+                name: name,
+                price: price
             }
         }
     );
