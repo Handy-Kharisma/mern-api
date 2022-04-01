3481dbac9a138340b76f9e07f4512b74dddfa9f1
Handy Kharisma <handykharisma75@gmail.com>
2022-04-01 10:49:54 +0700

    thirteenth commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const path = require('path');
+const cors = require('cors');
 // var bodyParser = require('body-parser')
 
 const app = express();
@@ -29,16 +30,30 @@ const fileFilter = function(req, file, cb){
     }
 }
 
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+
+app.use(cors())
 app.use(bodyParser.json()) //type JSON
 app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 
 app.use(function(req, res, next){
+    
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
+    // res.setHeader("Access-Control-Max-Age", "1800");  
     res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    // res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // res.setHeader("Access-Control-Allow-Headers", "content-type");
     next();
 });
+
 
 // app.use('/v1/customer', productRoutes);
 // app.use('/v2/customer', anotherRoutes);
